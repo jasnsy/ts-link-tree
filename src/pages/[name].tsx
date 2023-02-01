89f085129cb38ae532f565ebfc6062fa86b25d52
Jason <jason@Jasons-MacBook-Pro.local>
2023-02-01 10:17:44 -0700

    fix loading state

--- src/pages/[name].tsx
+++ src/pages/[name].tsx
@@ -9,12 +9,29 @@ function LinksPage() {
   function getUser() {
     const name = router?.query?.name as string;
 
-    const { data } = api.links.getLinks.useQuery({ name: name }, { enabled: router.isReady });
+    const { data, isLoading, isError } = api.links.getLinks.useQuery({ name: name }, { enabled: router.isReady });
+
+    if (isLoading) return (
+      <>
+      <div className="mx-auto mt-16 flex w-full flex-col items-center justify-center">
+          <h1 className="mt-4 text-xl font-bold text-white">Loading...</h1>
+      </div>
+      </>
+    )
+
+    if (isError) return (
+      <>
+      <div className="mx-auto mt-16 flex w-full flex-col items-center justify-center">
+          <h1 className="mt-4 text-xl font-bold text-white">Something went wrong...</h1>
+          <h2 className="mt-4 text-sm font-bold text-white">Please check back later</h2>
+      </div>
+      </>
+    )
 
     if (!data) return (
       <>
       <div className="mx-auto mt-16 flex w-full flex-col items-center justify-center">
-          <h1 className="mt-4 text-xl font-bold text-white">No User Found</h1>
+          <h1 className="mt-4 text-xl font-bold text-white">No user found...</h1>
       </div>
       </>
     )
