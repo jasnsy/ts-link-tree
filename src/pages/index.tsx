5191efb21aeccf527c8d89ac539f0d64ba02e91a
Jason <jason@Jasons-MacBook-Pro.local>
2023-02-01 19:37:47 -0700

    update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="LinkTray by Jason" />
+        <title>Links by Jason</title>
+        <meta name="description" content="Links by Jason" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
